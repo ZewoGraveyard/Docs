1b371193f526d8b0b66418e9ac51abebc9e13292
Paulo Faria <paulo.faria.rl@gmail.com>
2016-03-10 03:19:37 -0300

    add mouseflow.js

---
+++ mouseflow.js
@@ -0,0 +1,7 @@
+var _mfq = _mfq || [];
+(function() {
+    var mf = document.createElement("script");
+    mf.type = "text/javascript"; mf.async = true;
+    mf.src = "//cdn.mouseflow.com/projects/e7a386d3-cabb-491e-ae5a-37c76ad80cfd.js";
+    document.getElementsByTagName("head")[0].appendChild(mf);
+})();
